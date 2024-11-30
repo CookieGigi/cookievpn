cd51d98534996137324c22629b610a1ce0cb46f8
Cookiegigi <gcreusot@pm.me>
2024-11-30 12:29:19 +0800

    Add log

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::{
     net::{Shutdown, TcpListener, TcpStream},
 };
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use cli::Command;
 
 pub mod cli;
@@ -13,42 +13,47 @@ pub mod errors;
 #[cfg(not(tarpaulin_include))]
 pub fn run(command: Command) -> Result<()> {
     match command {
-        Command::Start => run_start(),
+        Command::Start => run_start()?,
     }
     Ok(())
 }
 
-pub fn run_start() {
+pub fn run_start() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
-        handle_connection(stream);
+        match handle_connection(stream) {
+            Ok(_) => continue,
+            Err(e) => tracing::error!("{e}"),
+        };
     }
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+#[tracing::instrument]
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    tracing::info!("Connection established!");
     let mut data = [0_u8; 50];
     while match stream.read(&mut data) {
         Ok(size) => {
-            println!("Data send back : {size}");
-            let s = match std::str::from_utf8(&data) {
-                Ok(v) => v,
-                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            };
-            println!("{s}");
+            tracing::debug!("Data send back : {size}");
             Write::write(&mut stream, &data[0..size]).unwrap();
             size != 0
         }
         Err(_) => {
-            println!(
+            stream.shutdown(Shutdown::Both).unwrap();
+            tracing::error!(
                 "An error occurred, terminating connection with {}",
                 stream.peer_addr().unwrap()
             );
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
+            bail!(
+                "An error occurred, terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
         }
     } {}
+    tracing::info!("Connection finished !");
+    Ok(())
 }
