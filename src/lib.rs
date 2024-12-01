0d2ad75d6f0441072b4b4b197f7ade07c20197ab
Cookiegigi <gcreusot@pm.me>
2024-12-01 22:46:25 +0800

    Add better log

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
+use uuid::Uuid;
 
 pub mod cli;
 pub mod config;
@@ -25,8 +26,10 @@ async fn run_start() -> Result<()> {
     loop {
         let stream = listener.accept().await?;
 
+        let request_id = Uuid::new_v4();
+
         tokio::spawn(async move {
-            match handle_connection(stream.0).await {
+            match handle_connection(stream.0, request_id).await {
                 Ok(_) => (),
                 Err(e) => tracing::error!("{e}"),
             };
@@ -36,7 +39,7 @@ async fn run_start() -> Result<()> {
 }
 
 #[tracing::instrument]
-async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+async fn handle_connection(mut stream: TcpStream, id: Uuid) -> Result<()> {
     tracing::info!("Connection established!");
     let mut data = [0_u8; 50];
     while match stream.read(&mut data).await {
