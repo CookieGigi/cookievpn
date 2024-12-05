178060d49d8d28038709dca85013f7344f76842f
Cookiegigi <gcreusot@pm.me>
2024-12-05 16:07:18 +0800

    ListenTUN : initialize a tun interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
+use std::{net::IpAddr, str::FromStr};
 
 use anyhow::{bail, Result};
 use cli::Command;
+use futures::stream::TryStreamExt;
+use netlink_packet_route::link::LinkMessage;
+use rtnetlink::{new_connection, Handle};
+use tun::{AbstractDevice, Device};
 
 pub mod cli;
 pub mod config;
 pub mod errors;
+pub mod tcp_echo;
 
 #[cfg(not(tarpaulin_include))]
 pub async fn run(command: Command) -> Result<()> {
@@ -16,7 +22,19 @@ pub async fn run(command: Command) -> Result<()> {
 }
 
 async fn run_start() -> Result<()> {
+    let name = "tun0";
+    let ip = "10.0.0.1";
+    let device = add_tun_device(name, IpAddr::from_str(ip)?)?;
+    let mut stdin = std::io::stdin();
+    let _ = std::io::Read::read(&mut stdin, &mut [0u8])?;
     Ok(())
 }
 
+fn add_tun_device(name: &str, ip: IpAddr) -> Result<Device> {
+    let mut config = tun::Configuration::default();
+    config.tun_name(name);
+    let mut tun_device = tun::create(&config)?;
+    tun_device.set_address(ip)?;
+    tun_device.enabled(true)?;
+    Ok(tun_device)
 }
